645412753215a645ceb8eb05a4bdcaccea26441f
Shital_Ajit_Shelar <107055765+sas2055@users.noreply.github.com>
2022-07-11 20:21:10 +0530

    flow_3.py

--- flow_3.py
+++ flow_3.py
@@ -3,7 +3,7 @@
 s = 'Shital Shelar'
 expected = 'Shelar Shital'
 for i in (s.split()[::-1]):
-    print(i,end= ' ')
+    print(i, end=' ')
 ===================================================
 
 # Que.2 reverse the character/blocks from string
@@ -22,37 +22,36 @@ print(final)
 s = 'Shital Shelar'
 print(s[4])
 # -1 to -n
-for i in range(-1,-(len(s)+1),-1):
-    # print (i,end=' ')
-    print(s[i],end='')
+for i in range(-1, -(len(s)+1), -1):
+    print (i, end=' ')
 
 0r
 s = 'Shital Shelar'
-for i in range(-1,-(len(s)+1),-1):
-    print(s[i],end='')
+for i in range(-1, -(len(s)+1), -1):
+    print(s[i], end='')
 ==============================================
 
 # Que.4 reverse the character 0f individual word
 s = 'Shital Shelar'
 expected = 'latihS ralehS '
 for i in s.split():
-    print(i[::-1],end=' ')
+    print(i[::-1], end=' ')
 =============================================
 
-# Que.5 i want to fetch vowels from the string: a,i,e,o,u
+# Que.5 i want to fetch vowels from the string: a, i, e, o, u
 s = 'Shital Shelar'
-vowels = ['a','e','i','o','u','A','E','I','O','U']
+vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
 for i in s:
     if i in vowels:
-     print(i,end=' ')
+     print(i, end=' ')
 ==================================================
 
 # Que.6 i want a dict of each block and its count
-# {'S':2,'h':2,'i':1,'t':1,'a':2 .............}
+# {'S':2, 'h':2, 'i':1, 't':1, 'a':2 .............}
 s = 'Shital Shelar'
 d = {}
 for i in s:
-    # print(i,s.count(i))
+    # print(i, s.count(i))
     # d[key] = value
     d[i] = s.count(i)
 print(d)
@@ -62,39 +61,40 @@ print(d)
 s = 'Shital Shelar'
 expected = 'lartal SheShi'
 for i in s:
-    if (len(s)>2):
+    if (len(s) > 2):
         pass
-print(s[-3:13]+s[3:-3]+s[:3],end= ' ')
+print(s[-3:13] + s[3:-3] + s[:3], end=' ')
 =============================================
 
 # Que.8 my key = 3
 # means i want to shift character by 3
 s = 'Shital Shelar'
 for i in s:
-    shifting_char = chr(ord(i)+3)
-    print(shifting_char,end= ' ')
+    shifting_char = chr(ord(i) + 3)
+    print(shifting_char, end=' ')
 
 or
 s = 'Shital Shelar'
 for i in s:
     a = ord(i) + 3
     b = chr(a)
-    print(b,end= ' ')
+    print(b, end=' ')
 
 or
 s = 'Shital Shelar'
 for i in s:
     if(i.isalpha()):
-        print(chr(ord(i)+3),end= ' ')
+        print(chr(ord(i) + 3), end= ' ')
     else:
-        print(' ',end='')
+        print(' ', end='')
 ====================================
 # Que.9 print the positive and negative of each block
 s = 'Shital Shelar'
 j = len(s)
 for i, k in zip(s, range(j)):
     if i == ' ':
-        print("' '",end = '')
-    print(i, 'position is:',(k ,-j))
+        print("' '", end ='')
+    print(i, 'position is: ', (k ,-j))
     j -= 1
+============================================    
 """
