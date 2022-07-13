be2fa954e2c38ae10f766b0d533999e197a45c2b
Shital_Ajit_Shelar <107055765+sas2055@users.noreply.github.com>
2022-07-13 23:07:38 +0530

    flow_5.py

---
+++ flow_5.py
@@ -0,0 +1,51 @@
+"""
+# iterative statements:
+    - it is also known as looping statements or repetitive statements.
+    - it is used to execute a part of the program as a given condition is True.
+---------------------------------------------------------------------
+
+# while loop:
+    - it is used to iterate over a block of code as long
+     as the test expression (condition) is true.
+--------------------------------------------------------
+syntax:
+    while <condition>
+        statements
+----------------------------------------------------------
+
+# Que.1 WAP to print python 5 times
+st = 'python'
+count = 1
+while count <= 5:
+    print(st)
+    count += 1
+=====================================================
+
+# Que.2 WAP to print 1 to 10 natural number
+num = 1
+while num < 11:
+    print(num)
+    num += 1
+=====================================================
+
+# Que.3 WAP to print 1 to 10 odd number
+num = 1
+while num < 11:
+    print(num)
+    num += 2
+======================================================
+
+# Que.4 WAP to print 1 to 10 even number
+num = 1
+while num < 11:
+    print(num +1)
+    num += 2
+=======================================================
+
+# Que.5 WAP to print shital
+num = 1
+while num == 1:
+    print('shital')
+    num = 2
+========================================================
+"""
